1a1806740d05371e182d1c9d31b4ed45e4b0fdc0
Bartek Nowak <barteknowak31l@gmail.com>
2023-10-29 13:09:56 +0100

    Added project files

---
+++ target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"put.io.selenium.atm","l":"AtmCardInfo"},{"p":"put.io.selenium.atm","l":"AtmServlet"}];updateSearchResults();
---
+++ target/site/testapidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"put.io.selenium.atm","l":"AtmCardInfoTest"}];updateSearchResults();
